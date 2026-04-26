8dff16b9e0ca580135c6d83afdc4f723b2b420f1
rsalehin <rsalehin@gmail.com>
2026-04-26 22:36:52 +0200

    Add cloneRepo function (shallow clone to tmp dir)

---
+++ src/core/clone.ts
@@ -0,0 +1,11 @@
+﻿import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import simpleGit from 'simple-git';
+
+export async function cloneRepo(url: string): Promise<string> {
+  const tmpDir = fs.mkdtempSync(path.join(os.tmpdir(), 'repo2rag-'));
+  const git = simpleGit();
+  await git.clone(url, tmpDir, ['--depth', '1']);
+  return tmpDir;
+}
